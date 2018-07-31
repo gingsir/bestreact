844cd8889b595d4597c178775b89d9449b4ee0cc
李景山 <jingshan.li@foxmail.com>
2018-07-31 21:43:58 +0800

    修改入口页面

--- src/entries/index/template.jsx
+++ src/entries/index/template.jsx
@@ -3,25 +3,18 @@ export default{
     render : function(vm){
         return (
             <div>
-                <h1>Hello React jsx scss files!</h1>
+                <h1>Hello bestreact!</h1>
                 <br />
-                <i className={'glyphicon glyphicon-console'}></i>
+                <i className={'glyphicon glyphicon-flash'}></i>
 
-                <div className="gf">
-                    <button type="button" className="form-control dropdown btn btn-default pct100" data-toggle="dropdown"
-                            name="ORDER_TYPE_STATUS" selected-value="ALL"><span className="value-text">全部</span><span
-                        className="caret"></span></button>
-                    <ul className="dropdown-menu dm-ORDER_TYPE_STATUS pct100">
-                        <li><a onClick={vm.buttonClick} value="ALL">全部</a></li>
-                        <li><a onClick={vm.buttonClick}  value="WAITBUYERPAY">待支付</a></li>
-                    </ul>
-                </div>
+				<div class="panel panel-default">
+                
+					<h2>vm.state.date is {vm.state.date.toLocaleTimeString()}.</h2>
 
-                <button onClick={vm.buttonClick}>click</button>
-
-                <h2>It is {vm.state.date.toLocaleTimeString()}.</h2>
-
-                <h2>It is {vm.props.age}.</h2>
+					<h2>vm.props.age is {vm.props.age}.</h2>
+				
+					<button className={"btn btn-success"} onClick={vm.buttonClick}>Click Me!</button>
+				</div>
             </div>
         )
     }
